7dcbce38e59fd00e41c9ec910b854b007453b929
Jonny Burger <jonathanburger11@gmail.com>
2019-06-21 21:03:04 +0200

    entry animation

--- src/lit-animation/Coin.js
+++ src/lit-animation/Coin.js
@@ -4,13 +4,15 @@ import { Svg, DangerZone } from 'expo'
 
 import formula from 'reanimated-formula'
 import styled from 'styled-components'
+import { darken } from 'polished'
+import { LinearGradient } from 'expo-linear-gradient'
 import { runTiming, atan2 } from 'react-native-redash'
 import { __COLORS } from '../layout/colors'
 
 const { Animated, Easing } = DangerZone;
 const { interpolate } = Animated
 
-const { Circle, LinearGradient, Stop, Defs, Filter, FEMerge, FEMergeNode, FEOffset, FEGaussianBlur } = Svg
+const { Circle, Stop, Defs, Filter, FEMerge, FEMergeNode, FEOffset, FEGaussianBlur } = Svg
 
 const Coin = styled(View)`
     width: 50px;
@@ -19,6 +21,18 @@ const Coin = styled(View)`
     justify-content: center;
     align-items: center;
 `;
+
+const CoinBackground = styled(Animated.createAnimatedComponent(LinearGradient)).attrs({
+    start: { x: 0, y: 0 },
+    end: { x: 1, y: 1 },
+    colors: ['white', darken(0.2, '#ffffff')]
+})`
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    position: absolute;
+`;
+
 
 const CoinIcon = styled(Animated.Image)`
 
@@ -85,6 +99,10 @@ export default class extends React.Component {
             inputRange: [0.0, 0.98, 1],
             outputRange: [0.5, 1, 0]
         })
+        const coinOpacity = interpolate(progress, {
+            inputRange: [0.9, 1],
+            outputRange: [0, 1]
+        })
         const { add, multiply, cos, sin } = Animated
         const translateX = add(multiply(radius, cos(alpha)), radius)
         const translateY = formula`-1 * ${radius} * sin(${alpha}) + ${radius}`
@@ -95,7 +113,7 @@ export default class extends React.Component {
                     scale: formula`min(${scale}, 1.2)`
                 }]
             }}>
-
+                <CoinBackground style={{ opacity: coinOpacity }}></CoinBackground>
                 <Coin>
                     <Dot
                         shadowColor="white"
