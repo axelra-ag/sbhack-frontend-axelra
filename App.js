2749b5ca993032f8c6355d9c8c46ccf512c7af35
Jonny Burger <jonathanburger11@gmail.com>
2019-06-21 17:02:51 +0200

    3 Tabs

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Tab1 from './src/Tab1';
+import Tab2 from './src/Tab2';
+import Tab3 from './src/Tab3';
+
+const TabNavigator = createBottomTabNavigator({
+  Tab1: Tab1,
+  Tab2: Tab2,
+  Tab3: Tab3
+})
+
+const Container = createAppContainer(TabNavigator)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container />
   );
 }
 
---
+++ src/Tab3.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Tab 3</Text>
+        </View>
+    )
+}
