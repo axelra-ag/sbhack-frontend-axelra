e6e0d7cc528432432527a8c77fcbd476a360bc3d
Jonny Burger <jonathanburger11@gmail.com>
2019-06-21 17:50:18 +0200

    Update Coin.js

--- src/lit-animation/Coin.js
+++ src/lit-animation/Coin.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import { View } from 'react-native'
+import { View, Image } from 'react-native'
 import styled from 'styled-components'
 
 const Coin = styled(View)`
-    background-color: green;
-    width: 25px;
-    height: 25px;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    justify-content: center;
+    align-items: center;
+    border-width: 2px;
 `;
+
+const CoinIcon = styled(Image)`
+
+`
+
+const ether = require('../../assets/coins/ether.png')
+const modum = require('../../assets/coins/modum.png')
+const vetri = require('../../assets/coins/vetri.png')
+
+const getSource = (type) => {
+    if (type == 'ether') {
+        return ether;
+    }
+    if (type === 'modum') {
+        return modum
+    }
+    if (type == 'vetri') {
+        return vetri
+    }
+}
 
 export default class extends React.Component {
     render() {
         return (
             <View>
-                <Coin></Coin>
+                <Coin>
+                    <CoinIcon style={{
+                        width: 32,
+                        height: 32
+                    }} source={ether} />
+                </Coin>
+                <Coin>
+                    <CoinIcon style={{
+                        width: 32,
+                        height: 32
+                    }} source={modum} />
+                </Coin>
+                <Coin>
+                    <CoinIcon style={{
+                        width: 32,
+                        height: 32
+                    }} source={vetri} />
+                </Coin>
             </View>
         )
 
