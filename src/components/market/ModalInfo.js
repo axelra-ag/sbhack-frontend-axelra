4cfd70c1ecf7e59a623db29aac00c0c23309394e
Chebotayev <pavel.chebotaev@efirst.ch>
2019-06-22 01:22:35 +0200

    add styles for first step on modal

--- src/components/market/ModalInfo.js
+++ src/components/market/ModalInfo.js
@@ -1,23 +1,77 @@
-import React from "react";
-import { Modal, Text, TouchableWithoutFeedback, View } from "react-native";
+import React, { Component } from "react";
+import {
+  Modal,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  Image,
+  Button,
+  TouchableOpacity
+} from "react-native";
 import styled from "styled-components";
+import { H3, Paragraph } from "../../layout/typography";
+import { __COLORS } from "../../layout/colors";
+import { Flex } from "../../layout/layout";
 
-export default props => {
-  return (
-    <Modal visible={props.visible} animationType="fade" transparent={false}>
-      <View style={{ marginTop: 22 }}>
-        <View>
-          <Text />
+const ModalWrapper = styled(View)`
+  padding: 30px;
+  align-items: center;
+  justify-content: center;
+`;
 
-          <TouchableWithoutFeedback
-            onPress={() => {
-              props.handleModal();
-            }}
-          >
-            <Text>Hide Modal</Text>
-          </TouchableWithoutFeedback>
-        </View>
-      </View>
-    </Modal>
-  );
-};
+const ButtonWrapper = styled(View)`
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center
+`
+export default class ModalInfo extends Component {
+  state = {
+    step: "first"
+  };
+
+  changeStep = step => {
+    this.setState({ step });
+  };
+
+  render() {
+    const {
+      visible,
+      handleModal,
+      clickedCompany: { name, logo, info }
+    } = this.props;
+
+    return (
+      <Modal visible={visible} animationType="fade" transparent={false}>
+        <ModalWrapper style={{ marginTop: 22 }}>
+          <H3>{name}</H3>
+          <Image style={{ width: 100, height: 100 }} source={logo} />
+          <Paragraph>{info}</Paragraph>
+          <ButtonWrapper>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                handleModal();
+              }}
+            >
+              <Text>Cancel</Text>
+            </TouchableWithoutFeedback>
+            <TouchableOpacity
+              style={{
+                alignItems: "center",
+                backgroundColor: __COLORS.SECOND,
+                paddingTop: 15,
+                paddingBottom: 15,
+                paddingLeft: 25,
+                paddingRight: 25,
+                borderRadius: 20
+              }}
+              onPress={() => this.changeStep("second")}
+            >
+              <Text> Order </Text>
+            </TouchableOpacity>
+          </ButtonWrapper>
+        </ModalWrapper>
+      </Modal>
+    );
+  }
+}
