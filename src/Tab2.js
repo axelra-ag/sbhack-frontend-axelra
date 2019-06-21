6c45942ccaecb451490920736ae2c2b030808c52
Jonny Burger <jonathanburger11@gmail.com>
2019-06-21 23:17:58 +0200

    lit

--- src/Tab2.js
+++ src/Tab2.js
@@ -5,40 +5,53 @@ import Coin, { size } from './lit-animation/Coin';
 import { LinearGradient } from 'expo-linear-gradient';
 import { __COLORS, __GRAY_SCALE } from './layout/colors';
 import { SafeAreaView } from 'react-navigation';
-import Header from "./wallet/Header";
+import AnimateNumber from 'react-native-animate-number';
+
+import Header from './wallet/Header';
 import { darken } from 'polished';
 import { __FONTS } from './layout/fonts';
 
-
 const Title = styled(Text)`
-    color: black;
-    padding-top: 30px;
-    font-size: 28px;
-    text-align: center;
-    font-weight: bold;
+	color: black;
+	padding-top: 30px;
+	font-size: 28px;
+	text-align: center;
+	font-weight: bold;
 `;
 
 const GoalAchived = styled(LinearGradient)`
-    padding: 15px;
-    padding-horizontal: 20px;
-    margin-left: 30px;
-    margin-right: 30px;
-    margin-top: 20px;
-    background: white;
-    border-radius: 2px;
+	padding: 15px;
+	padding-horizontal: 20px;
+	margin-left: 30px;
+	margin-right: 30px;
+	margin-top: 20px;
+	background: white;
+	border-radius: 2px;
 `;
 
 const Label = styled(Text)`
-    font-family: ${__FONTS.SEMIBOLD};
-    font-size: 15px;
-    color: ${__COLORS.FIRST};
+	font-family: ${__FONTS.SEMIBOLD};
+	font-size: 15px;
+	color: ${__COLORS.FIRST};
 `;
 
 export default class extends React.Component {
-    animationArea = React.createRef()
+    animationArea = React.createRef();
     state = {
         width: null,
-        height: null
+        height: null,
+        modum: 0,
+        valid: 0,
+        eth: 0
+    };
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                modum: 20,
+                valid: 20,
+                eth: 0.1
+            })
+        }, 10000);
     }
     render() {
         return (
@@ -53,28 +66,35 @@ export default class extends React.Component {
                         title={'Congratulations'}
                         subTitle={'Here are your rewards'}
                     />
-                    <View shadowColor="black"
+                    <View
+                        shadowColor="black"
                         shadowOffset={{
                             width: 0,
                             height: 0
                         }}
                         shadowOpacity={0.15}
-                        shadowRadius={2}>
-
+                        shadowRadius={2}
+                    >
                         <GoalAchived
-                            colors={["white", "white"]}
+                            colors={['white', 'white']}
                             start={{ x: 0, y: 0 }}
                             end={{ x: 0, y: 0 }}
-
                             style={{ flexDirection: 'row', alignItems: 'center' }}
                         >
-                            <Image style={{
-                                height: 24, width: 24, marginRight: 14,
-                                tintColor: __COLORS.THIRD
-                            }} source={require('../assets/bullseye.png')}></Image>
+                            <Image
+                                style={{
+                                    height: 24,
+                                    width: 24,
+                                    marginRight: 14,
+                                    tintColor: __COLORS.THIRD
+                                }}
+                                source={require('../assets/bullseye.png')}
+                            />
                             <View>
                                 <Label style={{ color: __COLORS.THIRD }}>Goal achieved</Label>
-                                <Label style={{ fontFamily: __FONTS.REGULAR }}>Bike 1 million kilometers</Label>
+                                <Label style={{ fontFamily: __FONTS.REGULAR }}>
+                                    Bike 1 million kilometers
+								</Label>
                             </View>
                         </GoalAchived>
                     </View>
@@ -86,8 +106,8 @@ export default class extends React.Component {
                                 this.setState({
                                     width,
                                     height
-                                })
-                            })
+                                });
+                            });
                         }}
                         style={{
                             flex: 1,
@@ -98,27 +118,46 @@ export default class extends React.Component {
                         <View style={{ flexDirection: 'row' }}>
                             {this.state.height ? (
                                 <>
-                                    <Coin xOffset={size + 30} height={this.state.height} type="modum" delay={1500} />
+                                    <Coin
+                                        xOffset={size + 30}
+                                        height={this.state.height}
+                                        type="modum"
+                                        delay={1500}
+                                    />
                                     <View style={{ width: 30 }} />
-                                    <Coin exitOffset={100} xOffset={0} height={this.state.height} type="ether" delay={3500} />
+                                    <Coin
+                                        exitOffset={100}
+                                        xOffset={0}
+                                        height={this.state.height}
+                                        type="ether"
+                                        delay={3500}
+                                    />
                                     <View style={{ width: 30 }} />
-                                    <Coin exitOffset={200} xOffset={0 - size - 30} height={this.state.height} type="vetri" delay={5500} />
+                                    <Coin
+                                        exitOffset={200}
+                                        xOffset={0 - size - 30}
+                                        height={this.state.height}
+                                        type="vetri"
+                                        delay={5500}
+                                    />
                                 </>
                             ) : null}
                         </View>
                     </View>
                     <View style={{ height: 65 }} />
-                    <View shadowColor="black"
+                    <View
+                        shadowColor="black"
                         shadowOffset={{
                             width: 0,
                             height: 0
                         }}
                         shadowOpacity={0.15}
-                        shadowRadius={2}>
+                        shadowRadius={2}
+                    >
                         <GoalAchived
                             colors={[__COLORS.THIRD, __COLORS.SECOND]}
                             start={{ x: 0, y: 0 }}
-                            end={{ x: .7, y: 0 }}
+                            end={{ x: 0.7, y: 0 }}
                             shadowColor="black"
                             shadowOffset={{
                                 width: 0,
@@ -126,15 +165,58 @@ export default class extends React.Component {
                             }}
                             shadowOpacity={0.15}
                             shadowRadius={2}
-                            style={{ backgroundColor: __COLORS.THIRD, flexDirection: 'row', alignItems: 'center', position: 'absolute', bottom: 10, width: Dimensions.get('window').width - 60 }}
+                            style={{
+                                backgroundColor: __COLORS.THIRD,
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                position: 'absolute',
+                                bottom: 10,
+                                width: Dimensions.get('window').width - 60
+                            }}
                         >
-                            <Image style={{
-                                height: 24, width: 24, marginRight: 14,
-                                tintColor: 'white'
-                            }} source={require('../assets/wallet.png')}></Image>
+                            <Image
+                                style={{
+                                    height: 24,
+                                    width: 24,
+                                    marginRight: 14,
+                                    tintColor: 'white'
+                                }}
+                                source={require('../assets/wallet.png')}
+                            />
                             <View>
                                 <Label style={{ color: 'white' }}>Wallet</Label>
-                                <Label style={{ fontFamily: __FONTS.REGULAR, color: 'white', opacity: 0.8 }}>1 MOD      3 VLD       0.1 ETH</Label>
+                                <Label
+                                    style={{
+                                        fontFamily: __FONTS.REGULAR,
+                                        color: 'white',
+                                        opacity: 0.8
+                                    }}
+                                >
+                                    <AnimateNumber
+                                        timing="easeOut"
+                                        value={this.state.modum}
+                                        formatter={val => {
+                                            return parseFloat(val).toFixed(2);
+                                        }}
+                                    />{' '}
+                                    MOD{' '}
+                                    <AnimateNumber
+                                        timing="easeOut"
+                                        formatter={val => {
+                                            return parseFloat(val).toFixed(2);
+                                        }}
+                                        value={this.state.valid}
+                                    />{' '}
+                                    VLD{' '}
+                                    <AnimateNumber
+                                        timing="easeOut"
+                                        formatter={val => {
+                                            return parseFloat(val).toFixed(2);
+                                        }}
+                                        value={this.state.eth}
+                                    />{' '}
+                                    ETH
+								</Label>
                             </View>
                         </GoalAchived>
                     </View>
