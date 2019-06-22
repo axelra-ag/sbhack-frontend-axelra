db8fb39987bbaa57806622a295bfb2e5f5e27cfd
Jonny Burger <jonathanburger11@gmail.com>
2019-06-22 11:41:43 +0200

    blockchain.

--- src/wallet/SeedScreen.js
+++ src/wallet/SeedScreen.js
@@ -4,6 +4,12 @@ import { Flex } from "../layout/layout";
 import Header from "./Header";
 import Web3 from "web3";
 
+const web3 = new Web3(
+  new Web3.providers.HttpProvider('https://kovan.infura.io/v3/344e9e84c0804cd5a15f4dd4b9b49a1e')
+);
+
+web3.eth.getBlockNumber().then(console.log)
+
 const Container = styled(Flex)``;
 
 class SeedScreen extends Component {
